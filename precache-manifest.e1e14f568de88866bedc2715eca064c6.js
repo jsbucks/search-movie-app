35edfa90f9410d15b5832317332bb44ee855efdb
jsbucks <mjweb2267@gmail.com>
2020-07-07 18:51:58 -0400

    Updates

---
+++ precache-manifest.e1e14f568de88866bedc2715eca064c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943ed8eecaee73eafac0d2bdb9562f62",
+    "url": "/search-movie-app/index.html"
+  },
+  {
+    "revision": "bd62ad372248b1238717",
+    "url": "/search-movie-app/static/css/main.bab02a8f.chunk.css"
+  },
+  {
+    "revision": "43cdeb28ee00f9a874ee",
+    "url": "/search-movie-app/static/js/2.a0ebc93e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-movie-app/static/js/2.a0ebc93e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd62ad372248b1238717",
+    "url": "/search-movie-app/static/js/main.e36d5b01.chunk.js"
+  },
+  {
+    "revision": "9e489272b4df4bfd4227",
+    "url": "/search-movie-app/static/js/runtime-main.45124ef7.js"
+  }
+]);
