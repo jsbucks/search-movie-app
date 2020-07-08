30dfe64e75a5617fce6b4ef641b08225ecfef6e8
jsbucks <mjweb2267@gmail.com>
2020-07-08 00:06:00 -0400

    Adding Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import SearchMovies from './components/searchMovies';
+import ReactGA from 'react-ga';
 
 function App() {
+    useEffect(() => {
+      ReactGA.initialize('UA-172022121-1');
+      ReactGA.pageview(window.location.pathname);
+    }, []);
+
     return (
         <div className="App">
             <div className="container">
